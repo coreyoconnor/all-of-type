92236f05be073a26503d52fded8aee38e62e6455
Corey O'Connor <coreyoconnor@gmail.com>
2024-09-06 06:46:51 +0000

    more tests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -21,11 +21,29 @@ object Foo {
   val other: Seq[Zib] = AllOfType.derive
 }
 
+object Zob {
+  val x: String = "lkee"
+}
+
+trait T0
+object Zic extends T0
+object Zid extends T0
+
+@AllOfType
+class Zob {
+  val x: T0 = Zic
+  val y = Zid
+
+  val all: Seq[T0] = AllOfType.derive
+}
+
 @main def hello(): Unit =
   println("Hello world!")
   println(msg)
   println(Foo.all)
   println(Foo.other)
   println(Zib.count)
+  println((new Zob).all)
+  println(Zob.x)
 
 def msg = "I was compiled by Scala 3. :)"
