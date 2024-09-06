2bf846a0574d8078e1ac68f8cba4081637e8127b
Corey O'Connor <coreyoconnor@gmail.com>
2024-09-06 06:42:43 +0000

    simplify

--- src/main/scala/AllOfType.scala
+++ src/main/scala/AllOfType.scala
@@ -3,8 +3,6 @@ import scala.quoted.*
 import scala.annotation.StaticAnnotation
 import scala.annotation.MacroAnnotation
 import scala.annotation.experimental
-import scala.annotation.targetName
-import scala.annotation.varargs
 
 object AllOfType {
   val derive: Nothing =
@@ -21,56 +19,49 @@ class AllOfType extends MacroAnnotation {
     import quotes.reflect.*
 
     sealed trait StmtAction
-    case class Passthrough(stmt: Statement) extends StmtAction
-    case class AllSeq(valDef: ValDef, targetType: Type[?]) extends StmtAction
-    case class AllSeqTarget(valDef: ValDef, tpe: Type[?]) extends StmtAction
+    case class Pass(stmt: Statement) extends StmtAction
+    case class SeqToBuild(valDef: ValDef, innerType: Type[?]) extends StmtAction
+    case class ValToInclude(valDef: ValDef, valType: Type[?]) extends StmtAction
+
+    def updateBody(body: List[Statement]): List[Statement] = {
+
+      val actions = body map {
+        case valDef@ValDef(_, valType, Some(valImpl)) =>
+          valType.tpe.asType match {
+            case '[Seq[innerType]] => {
+              valImpl.asExpr match {
+                case '{AllOfType.derive} => SeqToBuild(valDef, Type.of[innerType])
+                case stmt => Pass(valDef)
+              }
+            }
+            case tpe => ValToInclude(valDef, tpe)
+          }
+        case stmt => Pass(stmt)
+      }
+
+      val allSeqs = actions.collect { case SeqToBuild(seqDef, innerType) =>
+        val targetSymbols = actions.collect {
+          case ValToInclude(valDef, targetType)
+            if TypeRepr.of(using targetType) <:< TypeRepr.of(using innerType) => valDef.symbol
+        }
+
+        val targetRefs = Varargs(targetSymbols.map(Ref(_).asExpr))
+        val targetRefSeqs = '{ Seq($targetRefs) }.asTerm
+
+        seqDef.symbol -> ValDef.copy(seqDef)(seqDef.name, seqDef.tpt, Some(targetRefSeqs))
+      }.toMap
+
+      body map { stmt =>
+        allSeqs.get(stmt.symbol) match {
+          case Some(replacement) => replacement
+          case None => stmt
+        }
+      }
+    }
 
     val updatedDef = definition match {
       case ClassDef(name, constructor, parents, self, body) =>
-        val targetType = TypeRepr.of[String].typeSymbol
-
-        val allOfType = body collect {
-          case valDef@ValDef(_, valType, Some(valImpl)) =>
-            valType.tpe.asType match {
-              case '[Seq[targetType]] => {
-                valImpl.asExpr match {
-                  case '{AllOfType.derive} => AllSeq(valDef, Type.of[targetType])
-                  case stmt => Passthrough(valDef)
-                }
-              }
-              case tpe => AllSeqTarget(valDef, tpe)
-            }
-          case stmt => Passthrough(stmt)
-        }
-
-        val onlyValSeqs = allOfType collect {
-          case AllSeq(valDef, targetType) => valDef -> targetType
-        }
-
-        val collectedDefs = onlyValSeqs.map { case (allSeqDef, allSeqType) =>
-          allSeqDef.symbol -> (
-            allSeqDef,
-            allOfType.collect {
-              case AllSeqTarget(valDef, targetType)
-                if TypeRepr.of(using targetType) <:< TypeRepr.of(using allSeqType) =>
-                  valDef.symbol
-            }
-          )
-        }.toMap
-
-        val updatedBody = body map { stmt =>
-          collectedDefs.get(stmt.symbol) match {
-            case Some((allSeqDef, names)) => {
-              val namesRefArgs = Varargs(names.map(Ref(_).asExpr))
-              val newRhs = '{ Seq($namesRefArgs) }.asTerm
-
-              ValDef.copy(allSeqDef)(allSeqDef.name, allSeqDef.tpt, Some(newRhs))
-            }
-            case None => stmt
-          }
-        }
-
-        ClassDef.copy(definition)(name, constructor, parents, self, updatedBody)
+        ClassDef.copy(definition)(name, constructor, parents, self, updateBody(body))
     }
 
     List(Some(updatedDef), companion).flatten
